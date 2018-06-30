4543c0621ad208a1517127685b13a5e91845c17b
yxj0312 <yanxiajun@hotmail.com>
2018-06-30 22:52:01 +0200

    Frontend Testing is a disaster

--- src/Notification.js
+++ src/Notification.js
@@ -1,4 +1,6 @@
 export default {
+    template:'<div>{{ message }}</div>',
+    
     data() {
         return {
             message: 'Hello World'
--- test/notification.js
+++ test/notification.js
@@ -1,7 +1,8 @@
-import Vue from 'vue';
+import Vue from 'vue/dist/vue.js';
 import test from 'ava';
 import Notification from '../src/Notification';
 
 test('that it renders a notification', t=>{
-    t.is(Notification.data().message,'Hello World');
-}); 
+    // t.is(Notification.data().message,'Hello World');
+    new Vue(Notification).$mount();
+});
