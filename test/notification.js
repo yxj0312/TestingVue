b43284e46e737f51f256eea6f291861cb816121d
yxj0312 <yanxiajun@hotmail.com>
2019-12-15 13:15:06 +0100

    Add test files

--- test/notification.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue/dist/vue.js';
-import test from 'ava';
-import Notification from '../src/Notification';
-
-test('that it renders a notification', t=>{
-    // t.is(Notification.data().message,'Hello World');
-    new Vue(Notification).$mount();
-});
