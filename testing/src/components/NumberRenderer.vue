474aeb0288b3e31ae4178fb67bf81b27da0d96c0
yxj0312 <yanxiajun@hotmail.com>
2020-01-14 21:27:17 +0100

    failed approach for jest expect installation

---
+++ testing/src/components/NumberRenderer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumberRenderer",
+
+  props: {
+    even: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
--- testing/tests/unit/Countdow.spec.js
+++ testing/tests/unit/Countdow.spec.js
@@ -1,45 +1,58 @@
-import { expect } from "expect";
+import { expect } from "jest";
 import { shallowMount } from "@vue/test-utils";
-import Countdown from "@/components/Countdown.vue";
-import moment from 'moment';
+// import Countdown from "@/components/Countdown.vue";
+import NumberRenderer from "@/components/NumberRenderer.vue";
+// import moment from 'moment';
 
-describe("Countdown", () => {
-  let wrapper;
 
-  beforeEach(() => {
-    // wrapper = mount(Countdown)
-    wrapper = shallowMount(Countdown, {
+describe("NumberRenderer", () => {
+  it("renders even numbers", () => {
+    const wrapper = shallowMount(NumberRenderer, {
       propsData: {
-        until: moment().add(10, "seconds")
+        even: true
       }
-    });
-    console.log(moment().add(10, "seconds"))
-  });
+    })
 
-  it("renders a countdown timer", () => {
-    wrapper = shallowMount(Countdown);
-    // wrapper.setProps({ until: moment().add(10, 'seconds') })
-    //  <countdown until="December 5 2017"
-    // console.log('!23');
-    see("10 secondes");
-  });
+    expect(wrapper.text()).toBe("2, 4, 6, 8")
+  })
+})
+// describe("Countdown", () => {
+//   let wrapper;
 
-  // Helper Functions
+//   beforeEach(() => {
+//     // wrapper = mount(Countdown)
+//     wrapper = shallowMount(Countdown, {
+//       propsData: {
+//         until: moment().add(10, "seconds")
+//       }
+//     });
+//     console.log(moment().add(10, "seconds"))
+//   });
 
-  let see = (text, selector) => {
-    let wrap = selector ? wrapper.find(selector) : wrapper;
+//   it("renders a countdown timer", () => {
+//     wrapper = shallowMount(Countdown);
+//     // wrapper.setProps({ until: moment().add(10, 'seconds') })
+//     //  <countdown until="December 5 2017"
+//     // console.log('!23');
+//     see("10 secondes");
+//   });
 
-    expect(wrap.html()).toContain(text);
-  };
+//   // Helper Functions
 
-  // it("renders even numbers", () => {
-  //   const wrapper = shallowMount(Countdown, {
-  //     propsData: {
-  //       even: true
-  //     }
-  //   });
+//   let see = (text, selector) => {
+//     let wrap = selector ? wrapper.find(selector) : wrapper;
 
-  //   // expect(wrapper.text()).to.include("123");
-  //   // expect(wrapper.text().includes('"123"')).toBe(true);
-  // });
-});
+//     expect(wrap.html()).toContain(text);
+//   };
+
+//   // it("renders even numbers", () => {
+//   //   const wrapper = shallowMount(Countdown, {
+//   //     propsData: {
+//   //       even: true
+//   //     }
+//   //   });
+
+//   //   // expect(wrapper.text()).to.include("123");
+//   //   // expect(wrapper.text().includes('"123"')).toBe(true);
+//   // });
+// });
